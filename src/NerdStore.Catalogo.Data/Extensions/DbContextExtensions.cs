2f35be152d2cf08a9b9e9e143428558b87592e63
Alexander Vieira da Silva <alexander.silva@al.infnet.edu.br>
2024-03-04 03:25:25 -0300

    feat[nerdstore::mvc]: macanismo para popular banco de dados automatico - Implementação método de extensão para migration e popular banco de dados via código. - Ajustes nas views home, vitrine, produto-detalhe e layout.

--- src/NerdStore.Catalogo.Data/Extensions/DbContextExtensions.cs
+++ src/NerdStore.Catalogo.Data/Extensions/DbContextExtensions.cs
@@ -4,7 +4,9 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NerdStore.Catalogo.Data.Constants;
+using NerdStore.Catalogo.Domain;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -26,9 +28,82 @@ namespace NerdStore.Catalogo.Data.Extensions
                   });
             });
            
-        }        
+        }
 
         public static void SeedCatalogoData(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<CatalogoContext>())
+                {
+                    ArgumentNullException.ThrowIfNull(context, nameof(context));
+                   
+                    context.Database.Migrate();
+
+                    if (!context.Produtos.Any())
+                    {
+                        using (var transaction = context.Database.BeginTransaction())
+                        {
+                            try
+                            {                                
+								var categoriaCamisa = new Categoria("Camisas", 100);
+								var categoriaCaneca = new Categoria("Canecas", 101);
+								var categoriaAdesivo = new Categoria("Adesivos", 102);								
+								var categoriaBone = new Categoria("Bones", 103);
+								var categoriaSmartphone = new Categoria("Smartphone", 104);
+								var categoriaIphone = new Categoria("Iphone", 105);
+
+								var produtosEspeciais = new List<Produto> 
+                                {
+                                    new Produto("Camiseta Developer", "Aliquam erat volutpat", true, 99.00M, categoriaCamisa.Id, DateTime.Now, "Camiseta1.jpg", new Dimensoes(5,5,5)),
+                                    new Produto("Camiseta Code", "Aliquam erat volutpat", true, 89.00M, categoriaCamisa.Id, DateTime.Now, "camiseta2.jpg", new Dimensoes(5,5,5)),
+                                    new Produto("Caneca StarBugs", "Aliquam erat volutpat", true, 49.00M, categoriaCaneca.Id, DateTime.Now, "caneca1.jpg", new Dimensoes(5,5,5)),
+                                    new Produto("Caneca Code", "Aliquam erat volutpat", true, 45.00M, categoriaCaneca.Id, DateTime.Now, "caneca2.png", new Dimensoes(5,5,5)),
+
+                                };
+                                
+                                context.Produtos.AddRange(produtosEspeciais);
+                                context.SaveChanges();
+
+                                var produtosDestaque = new List<Produto>
+                                {
+                                    new Produto("IPhone*", "Aliquam erat volutpat *", true, 1998.00M, categoriaIphone.Id, DateTime.Now, "iphone.png", new Dimensoes(5,5,5)),
+                                    new Produto("Samsung Galaxy S4*", "Aliquam erat volutpat *", true, 1199.00M, categoriaSmartphone.Id, DateTime.Now, "galaxy-s4.jpg", new Dimensoes(5,5,5)),
+                                    new Produto("Samsung Galaxy Note*", "Aliquam erat volutpat *", true, 1289.00M, categoriaSmartphone.Id, DateTime.Now, "galaxy-note.jpg", new Dimensoes(5,5,5)),
+                                    new Produto("Z1*", "Aliquam erat volutpat *", true, 1389.00M, categoriaSmartphone.Id, DateTime.Now, "Z1.png", new Dimensoes(5,5,5)),
+
+                                };
+
+                                context.Produtos.AddRange(produtosDestaque);
+                                context.SaveChanges();
+
+								var produtos = new List<Produto>
+								{
+									new Produto("IPhone", "Aliquam erat volutpat", true, 2998.00M, categoriaIphone.Id, DateTime.Now, "iphone.png", new Dimensoes(5,5,5)),
+									new Produto("Samsung Galaxy S4", "Aliquam erat volutpat", true, 989.00M, categoriaSmartphone.Id, DateTime.Now, "galaxy-s4.jpg", new Dimensoes(5,5,5)),
+									new Produto("Samsung Galaxy Note", "Aliquam erat volutpat", true, 1179.00M, categoriaSmartphone.Id, DateTime.Now, "galaxy-note.jpg", new Dimensoes(5,5,5)),
+									new Produto("Z1", "Aliquam erat volutpat", true, 1089.00M, categoriaSmartphone.Id, DateTime.Now, "Z1.png", new Dimensoes(5,5,5)),
+
+								};
+
+								context.Produtos.AddRange(produtos);
+								context.SaveChanges();
+
+								transaction.Commit();
+                            }
+                            catch
+                            {
+                                transaction.Rollback();
+                                throw;
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        public static void SeedCatalogoDataFromScripts(this IApplicationBuilder app, IConfiguration configuration)
         {
             using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
---
+++ src/NerdStore.WebApp.MVC/Areas/Identity/Data/Extensions/DbContextExtensions.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NerdStore.WebApp.MVC.Areas.Identity.Data;
+using NerdStore.WebApp.MVC.Constants;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace NerdStore.WebApp.MVC.Areas.Data.Extensions
+{
+	public static class DbContextExtensions
+    {
+        public static void AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString(ContextConstants.DB_CONNECTION_NAME) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString,
+                  x =>
+                  {
+                      x.MigrationsHistoryTable("__EFMigrationsHistory");
+                      x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.GetName().Name);
+                  });
+            });
+
+			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+		}
+
+        public static void SeedIdentityData(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>())
+                {
+                    ArgumentNullException.ThrowIfNull(context, nameof(context));
+                   
+                    context.Database.Migrate();
+                    
+                }
+            }
+        }
+
+        public static void SeedIdentityDataFromScripts(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>())
+                {
+                    ArgumentNullException.ThrowIfNull(context, nameof(context));
+                    if (context.Users.Any()) return;
+                    context.Database.Migrate();
+
+                    var assembly = typeof(DbContextExtensions).Assembly;
+                    var files = assembly.GetManifestResourceNames();
+
+                    var executedSeedings = context.SeedingEntries.ToArray();
+                    var filePrefix = $"{assembly.GetName().Name}.Seedings.";
+                    foreach (var file in files.Where(f => f.StartsWith(filePrefix) && f.EndsWith(".sql"))
+                                              .Select(f => new
+                                              {
+                                                  PhysicalFile = f,
+                                                  LogicalFile = f.Replace(filePrefix, String.Empty)
+                                              })
+                                              .OrderBy(f => f.LogicalFile))
+                    {
+                        if (executedSeedings.Any(e => e.Name == file.LogicalFile))
+                            continue;
+
+                        string command = string.Empty;
+                        using (Stream stream = assembly.GetManifestResourceStream(file.PhysicalFile))
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                command = reader.ReadToEnd();
+                            }
+                        }
+
+                        if (String.IsNullOrWhiteSpace(command))
+                            continue;
+
+                        using (var transaction = context.Database.BeginTransaction())
+                        {
+                            try
+                            {
+                                context.Database.ExecuteSqlRaw(command);
+                                context.SeedingEntries.Add(new Entities.SeedingEntry() { Name = file.LogicalFile });
+                                context.SaveChanges();
+                                transaction.Commit();
+                            }
+                            catch
+                            {
+                                transaction.Rollback();
+                                throw;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        
+    }
+}
